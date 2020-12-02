5b2d1b4e1c799437475c7720936d86ec8efeb0f4
didukhroma <70459354+didukhroma@users.noreply.github.com>
2020-12-02 11:02:29 +0200

    task -1 / 2 / 3

---
+++ js/task-3.js
@@ -0,0 +1,22 @@
+// // Напиши фукцнию findLongestWord(string), которая принимает параметром произвольную строку
+// // (в строке будут только слова и пробелы) и возвращает самое длинное слово в этой строке.
+
+const findLongestWord = function (string) {
+  const arrayString = string.split(' ');
+  let lengthWord = 0;
+  let indexLongWord = 0;
+  for (let i = 0; i < arrayString.length; i += 1) {
+    if (arrayString[i].length > lengthWord) {
+      lengthWord = arrayString[i].length;
+      indexLongWord = i;
+    } else {
+      continue;
+    }
+  }
+  const longWord = arrayString[indexLongWord];
+  return longWord;
+};
+
+console.log(findLongestWord('Google do a roll'));
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog'));
+console.log(findLongestWord('May the force be with you'));
